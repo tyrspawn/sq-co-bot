a232fb82c725ab3bf085c676494ecfd611898e5a
Bobby Moretti <bobmoretti@gmail.com>
2020-09-25 18:43:40 -0700

    Misc UX improvements for the script  - added description  - converted giant help strings to DMs instead of spamming the request channel  - made error messages snarkier  - added "summon" command

--- co.py
+++ co.py
@@ -16,19 +16,20 @@ def get_mod_path():
 client = discord.Client()
 
 audiodir = os.path.join(get_mod_path(), 'sounds')
-description = 'test'
-bot = dcmd.Bot(command_prefix='!', description=description)
+description = 'Kernels of wisdom from fighter pilot legends.'
+bot = dcmd.Bot(command_prefix='!co-', description=description)
 
-print(audiodir)
-
+print(f'{audiodir=}')
 
 if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+    if not discord.opus._load_default():
+        # the 'opus' library here is opus.dll on windows
+        # or libopus.so on linux in the current directory
+        # you should replace this with the location the
+        # opus library is located in and with the proper filename.
+        # note that on windows this DLL is automatically provided for you
+        print("Unable to find default location of libopus-0.so... trying opus.dll")
+        discord.opus.load_opus('opus')
 
 @bot.command(description='Send friendly salutations to this bot.')
 async def hello(ctx):
@@ -39,36 +40,48 @@ async def hello(ctx):
 @bot.command(description='Solicit some wisdom from the CO.')
 async def play(ctx, sound_name=''):
     if sound_name == '':
-        msg = 'You must specify a sound. It should be one of the following\n'
-        msg = '```\n'
+        msg = 'These are the topics I can tell you about:\n'
+        msg += '```\n'
         msg += '\n'.join(sounds)
         msg += '```'
-        await ctx.send(msg)
+        await ctx.author.send(msg)
     else:
         await play_sound(ctx, sound_name)
+
+async def join_channel(ctx, channel, errmsg=None):
+    if ctx.voice_client is not None:
+        try:
+            await ctx.voice_client.move_to(channel)
+        except dcmd.errors.BadArgument:
+            if errmsg is not None:
+                await ctx.send(errmsg)
+    else:
+        try:
+            await channel.connect()
+        except dcmd.errors.BadArgument:
+            if errmsg is not None:
+                await ctx.author.send(errmsg)
 
 @bot.command(description="Join the given voice channel.")
 async def join(ctx, *, channel: discord.VoiceChannel):
     """Joins a voice channel"""
     errmsg = "Can't find channel by the name of {}".format(channel)
-    if ctx.voice_client is not None:
-        try:
-            await ctx.voice_client.move_to(channel)
-        except dcmd.errors.BadArgument:
-            await ctx.send(errmsg)
+    await join_channel(ctx, channel, errmsg)
+
+@bot.command(description="Join the user's voice channel.")
+async def summon(ctx):
+    if ctx.author.voice and ctx.author.voice.channel:
+        channel = ctx.author.voice.channel
+        await join_channel(ctx, channel)
     else:
-        try:
-            await channel.connect()
-        except dcmd.errors.BadArgument:
-            await ctx.send(errmsg)
-            
+        await ctx.message.send("You are not connected to a voice channel")
 
 @bot.command(description="Leave any connected voice channel.")
 async def leave(ctx):
     try:
         await ctx.voice_client.disconnect()
     except AttributeError:
-        await ctx.send("I'm not in any voice channel on this server!")
+        await ctx.author.send("I'm not in any voice channel on this server!")
 
 @bot.command(description="Stops any audio being played.")
 async def stop(ctx):
@@ -78,14 +91,14 @@ async def play_sound(ctx, name):
     try:
         fname = '.'.join([os.path.join(audiodir, name), 'ogg'])
         if not os.path.exists(fname):
-            await ctx.send("```Can't find audio file '{}'```".format(name))
+            await ctx.author.send(f"I don't know anything about ```{name}```")
             return
         
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(fname))
         try:
             ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
         except AttributeError:
-            await ctx.send('I need to be in a voice channel to do that!')
+            await ctx.author.send('I need to be in a voice channel to do that!')
 
     except Exception as e:
         fmt = 'An error occurred while processing this request: ```py\n{}: {}\n```'
