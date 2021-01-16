d6e2cfe137f4328db5309e50d81e4b15ed239cdf
Chris Krause <krause@krauselabs.net>
2021-01-15 21:35:11 -0800

    blah

--- co.py
+++ co.py
@@ -7,12 +7,6 @@ import json
 
 import inspect
 
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 5
-
 def get_mod_path():
     filepath = os.path.abspath(__file__)
     dirname, fname = os.path.split(filepath)
